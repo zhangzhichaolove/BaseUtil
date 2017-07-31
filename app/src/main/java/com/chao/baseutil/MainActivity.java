0a485fbb74ed3307fc3e7806dfe212ece00e1cc5
zhangdong <l>
2017-07-31 17:36:46 +0800

    change back

--- app/src/main/java/com/chao/baseutil/MainActivity.java
+++ app/src/main/java/com/chao/baseutil/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initView() {
         toolbar.setTitle("首页");
-
         toolbar.setRightImg(R.mipmap.s11);
         LogUtils.showTagE("-----" + GeneralVar.getScreenWidth());
         LogUtils.showTagE("-----" + GeneralVar.getScreenHeight());
